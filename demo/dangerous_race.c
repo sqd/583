080ac26565271f551aa19e81cd7bcad479068237
Han Y <me@hanyou.dev>
2020-12-14 18:54:01 -0500

    add race demo

---
+++ demo/dangerous_race.c
@@ -0,0 +1,18 @@
+#include <omp.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main() {
+    int sum = 0;
+#pragma omp parallel
+    while (1) {
+        int tid = omp_get_thread_num();
+        if (tid % 2)
+            sum++;
+        else
+            sum--;
+    }
+
+}
+
---
+++ demo/safe_race.c
@@ -0,0 +1,21 @@
+#include <omp.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main() {
+    omp_lock_t lock;
+    int sum = 0;
+#pragma omp parallel
+    while (1) {
+        int tid = omp_get_thread_num();
+        omp_set_lock(&lock);
+        if (tid % 2)
+            sum += 233;
+        else
+            sum -= 666;
+        omp_unset_lock(&lock);
+    }
+
+}
+
